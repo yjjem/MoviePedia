8030997f7f48184025205f01f7fb586d007acb82
yjjem <marksim0606@gmail.com>
2023-04-05 00:31:01 +0900

    test: add MockURLProtocol

---
+++ remy-movie/NetwokrPlatformTest/MockURLProtocol.swift
@@ -0,0 +1,40 @@
+//
+//  MockURLProtocol.swift
+//  remy-movie
+//
+//  Copyright (c) 2023 Jeremy All rights reserved.
+    
+
+import Foundation
+
+final class MockURLProtocol: URLProtocol {
+    
+    static var requestHandler: ((URLRequest) throws -> (Data, HTTPURLResponse))?
+    
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+
+    override func startLoading() {
+        
+        guard let requestHandler = MockURLProtocol.requestHandler else {
+            fatalError("FatalError: missing request handler")
+        }
+        
+        do {
+            let (data, response) = try requestHandler(request)
+            print(data, response)
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            client?.urlProtocol(self, didLoad: data)
+            client?.urlProtocolDidFinishLoading(self)
+        } catch {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+    }
+    
+    override func stopLoading() { }
+}
