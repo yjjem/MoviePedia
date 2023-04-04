56efcd51fa9e74335b712e1291d6be4fd5df71c4
yjjem <marksim0606@gmail.com>
2023-04-05 00:25:37 +0900

    docs: add NetwokrPlatformTest

---
+++ remy-movie/NetwokrPlatformTest/NetwokrPlatformTest.swift
@@ -0,0 +1,15 @@
+//
+//  NetwokrPlatformTest.swift
+//  remy-movie
+//
+//  Copyright (c) 2023 Jeremy All rights reserved.
+
+
+import XCTest
+
+final class NetwokrPlatformTest: XCTestCase {
+    
+    override func setUpWithError() throws { }
+    
+    override func tearDownWithError() throws { }
+}
