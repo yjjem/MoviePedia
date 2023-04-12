e18613d0ca20fe0065388ef67a80ba24d9c4ce79
yjjem <marksim0606@gmail.com>
2023-04-12 16:38:22 +0900

    feat(Service): TmdbAPIDetails 네임스페이스 추가

---
+++ remy-movie/remy-movie/Data/Service/TmdbAPIDetails.swift
@@ -0,0 +1,15 @@
+//
+//  TmdbAPIInfo.swift
+//  remy-movie
+//
+//  Copyright (c) 2023 Jeremy All rights reserved.
+    
+
+import Foundation
+
+enum TmdbAPIDetails {
+    static let defaultHost = "https://api.themoviedb.org"
+    static let defaultPath = "/3/movie"
+    static let imageHost = "https://image.tmdb.org"
+    static let imagePath = "/t/p"
+}
