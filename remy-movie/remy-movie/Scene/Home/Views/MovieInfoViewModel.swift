5dc0421e34e291c05c7b2faf10407eca2fc0f728
yjjem <marksim0606@gmail.com>
2023-04-27 17:38:36 +0900

    feat(Home): MovieInfoViewModel 추가

---
+++ remy-movie/remy-movie/Scene/Home/Views/MovieInfoViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  MovieInfoViewModel.swift
+//  remy-movie
+//
+//  Copyright (c) 2023 Jeremy All rights reserved.
+    
+
+import UIKit
+
+struct MovieInfoViewModel {
+    let backdropPath: String?
+    let posterPath: String?
+    let title: String
+    let userScore: Double
+    let movie: Movie
+    
+    init(movie: Movie) {
+        self.movie = movie
+        self.title = movie.title
+        self.userScore = movie.voteAverage
+        self.backdropPath = movie.backdropPath
+        self.posterPath = movie.backdropPath
+    }
+}
