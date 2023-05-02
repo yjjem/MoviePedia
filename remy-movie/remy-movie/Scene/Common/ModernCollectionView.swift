a85aa8aa21f681309af06d2d2b5455232a108b5c
yjjem <marksim0606@gmail.com>
2023-05-03 03:45:25 +0900

    feat(Home): TrendingMovieCollecionView 추가
    add ModerCollectionView

---
+++ remy-movie/remy-movie/Scene/Common/ModernCollectionView.swift
@@ -0,0 +1,65 @@
+//
+//  ModernCollectionView.swift
+//  remy-movie
+//
+//  Copyright (c) 2023 Jeremy All rights reserved.
+    
+
+import UIKit
+
+protocol ModernCollectionView: UICollectionView {
+    associatedtype Section: Sendable, Hashable
+    associatedtype SectionItem: Sendable, Hashable
+    associatedtype DiffableDatatSource = UICollectionViewDiffableDataSource<Section, SectionItem>
+    associatedtype LayoutConstants
+    
+    var diffableDataSource: DiffableDatatSource { get }
+}
+
+extension ModernCollectionView {
+    
+    func makeItem(
+        width: NSCollectionLayoutDimension,
+        height: NSCollectionLayoutDimension
+    ) -> NSCollectionLayoutItem {
+        
+        let itemSize = NSCollectionLayoutSize(widthDimension: width, heightDimension: height)
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        return item
+    }
+    
+    func makeHorizontalGroup(
+        width: NSCollectionLayoutDimension,
+        height: NSCollectionLayoutDimension,
+        repeatingItem: NSCollectionLayoutItem,
+        count: Int
+    ) -> NSCollectionLayoutGroup {
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: width, heightDimension: height)
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: groupSize,
+            repeatingSubitem: repeatingItem,
+            count: count
+        )
+        
+        return group
+    }
+    
+    func makeVerticalGroup(
+        width: NSCollectionLayoutDimension,
+        height: NSCollectionLayoutDimension,
+        repeatingItem: NSCollectionLayoutItem,
+        count: Int
+    ) -> NSCollectionLayoutGroup {
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: width, heightDimension: height)
+        let group = NSCollectionLayoutGroup.vertical(
+            layoutSize: groupSize,
+            repeatingSubitem: repeatingItem,
+            count: count
+        )
+        
+        return group
+    }
+}
